f150bad97b58ee832310c08e9f26cd7860b67d85
Santosh22570 <santu22570@gmail.com>
2026-03-13 12:58:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Taskform from "./Components/Taskform";
+import Tasklist from "./Components/Tasklist";
+import Progresstracker from "./Components/Progresstracker";
+
+export default function App() {
+  return (
+    <div>
+      <h1>TimeLine</h1>
+      <p>Our friendly TaskManager</p>
+      <Taskform />
+      <Tasklist />
+      <Progresstracker />
+      <button> Clear all tasks</button>
+    </div>
+  )
+}
