e06cc98aed0439ab76928085b7aba0b0c2133598
Santosh22570 <santu22570@gmail.com>
2026-03-27 17:30:47 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,53 @@
+
 import Taskform from "./Components/Taskform";
-import Tasklist from "./Components/Tasklist";
+import TaskList from "./Components/Tasklist";
 import Progresstracker from "./Components/Progresstracker";
+import { useEffect, useState } from "react";
+import "./Style.css";
+
 
 export default function App() {
-  return (
-    <div>
-      <h1>TimeLine</h1>
-      <p>Our friendly TaskManager</p>
-      <Taskform />
-      <Tasklist />
-      <Progresstracker />
-      <button> Clear all tasks</button>
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => { 
+    localStorage.setItem
+    ("tasks", JSON.stringify(tasks))
+  });
+
+  const addTask = (task) => {
+    setTasks([...tasks,task]);
+  }
+
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+    newtask[index] = updatedTask;
+    setTasks(newtask); // sretTasks is used to give newtask to tasks
+  }
+
+  const deleteTask = (index) => {
+      setTasks(tasks.filter((_, i) => i != index));
+  }
+
+  const clearTasks = () => {
+    setTasks([]);
+  }
+
+  return(
+    <div className="App">
+      <header>
+      <h1 className="title">Task Focus</h1>
+      <p className="tagline">Friendly TaskManager</p>
+      </header>
+      <Taskform addTask = {addTask}/>
+      <TaskList tasks = {tasks} 
+      updateTask = {updateTask}
+      deleteTask = {deleteTask}/>
+      <Progresstracker tasks = {tasks}/>
+
+      {tasks.length>0 && 
+      (<button onClick={clearTasks} className="clear-btn">Clear all tasks</button>)}
+      
     </div>
   )
 }
+
--- src/Components/Progresstracker.jsx
+++ src/Components/Progresstracker.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Progresstracker() {
+export default function Progresstracker({tasks}) {
   return (
     <div>
       <h1>Progress Tracker Component</h1>
